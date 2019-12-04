d62092de5eb39078b00bb7546c60b757a8909633
Guido Knips <guido.knips@posteo.de>
2019-12-04 10:46:49 +0100

    Add solution for day 3 puzzle 1
    - brute force (I was too lazy to think too much)

---
+++ day-03/src/main.rs
@@ -0,0 +1,210 @@
+use std::cmp;
+use std::collections::HashSet;
+use std::env;
+use std::fs::File;
+use std::io::{BufReader, Read};
+use std::path::Path;
+
+fn main() -> Result<(), String> {
+    let filename = env::args()
+        .nth(1)
+        .ok_or_else(|| "No file name given.".to_owned())?;
+    let content = read_file(&Path::new(&filename)).map_err(|e| e.to_string())?;
+    let lines: Vec<&str> = content.split('\n').collect();
+
+    let (wire1, wire2) = parse_wires(&lines)?;
+
+    if let Some(distance) = closest_crossing(&wire1, &wire2).map(|(x, y)| central_distance(x, y)) {
+        println!("Distance to closest crossing: {}", distance);
+    } else {
+        println!("No crossings found!");
+    }
+
+    Ok(())
+}
+
+fn read_file(path: &Path) -> std::io::Result<String> {
+    let ifile = File::open(path)?;
+    let mut bufr = BufReader::new(ifile);
+    let mut result = String::with_capacity(2048);
+    bufr.read_to_string(&mut result)?;
+
+    Ok(result)
+}
+
+#[derive(Clone, PartialEq, PartialOrd, Eq, Ord, Debug, Hash)]
+enum Turn {
+    Hor(i32),
+    Ver(i32),
+}
+
+fn parse_turn(turn: &str) -> Result<Turn, String> {
+    if turn.len() < 2 {
+        return Err(format!("Invalid turn: {}", turn));
+    }
+    if !turn.is_char_boundary(1) {
+        return Err(format!("Invalid turn: {}", turn));
+    }
+    let (dir, abs_raw) = turn.split_at(1);
+    let abs: i32 = abs_raw
+        .parse()
+        .map_err(|e| format!("Invalid turn: {}, error: {}", turn, e))?;
+
+    match dir {
+        "U" => Ok(Turn::Ver(abs)),
+        "R" => Ok(Turn::Hor(abs)),
+        "D" => Ok(Turn::Ver(-abs)),
+        "L" => Ok(Turn::Hor(-abs)),
+        _ => Err(format!("Invalid turn: {}", turn)),
+    }
+}
+
+fn parse_wire(line: &str) -> Result<Vec<Turn>, String> {
+    line.split(',')
+        .map(|t| t.trim())
+        .filter(|t| !t.is_empty())
+        .map(parse_turn)
+        .collect()
+}
+
+fn parse_wires(lines: &[&str]) -> Result<(Vec<Turn>, Vec<Turn>), String> {
+    let mut wires = lines.iter().map(|l| parse_wire(*l));
+
+    let wire1 = wires
+        .next()
+        .transpose()?
+        .ok_or_else(|| "No first wire.".to_owned())?;
+    let wire2 = wires
+        .next()
+        .transpose()?
+        .ok_or_else(|| "No second wire.".to_owned())?;
+
+    Ok((wire1, wire2))
+}
+
+fn wire_positions(wire: &[Turn]) -> HashSet<(i32, i32)> {
+    let mut wire_pos: HashSet<(i32, i32)> = HashSet::with_capacity(500);
+    let mut current_pos = (0, 0);
+    for turn in wire {
+        match turn {
+            Turn::Ver(length) => {
+                let (curr_x, curr_y) = current_pos;
+                for y in cmp::min(curr_y, curr_y + length)..=cmp::max(curr_y, curr_y + length) {
+                    wire_pos.insert((curr_x, y));
+                }
+                current_pos = (curr_x, curr_y + length);
+            }
+            Turn::Hor(length) => {
+                let (curr_x, curr_y) = current_pos;
+                for x in cmp::min(curr_x, curr_x + length)..=cmp::max(curr_x, curr_x + length) {
+                    wire_pos.insert((x, curr_y));
+                }
+                current_pos = (curr_x + length, curr_y);
+            }
+        };
+    }
+    wire_pos
+}
+
+fn wire_crossings(wire1: &[Turn], wire2: &[Turn]) -> HashSet<(i32, i32)> {
+    let wire_pos_1 = wire_positions(wire1);
+    let wire_pos_2 = wire_positions(wire2);
+
+    wire_pos_1
+        .intersection(&wire_pos_2)
+        .filter(|(x, y)| *x != 0 || *y != 0)
+        .cloned()
+        .collect()
+}
+
+fn central_distance(x: i32, y: i32) -> i32 {
+    x.abs() + y.abs()
+}
+
+fn closest_crossing(wire1: &[Turn], wire2: &[Turn]) -> Option<(i32, i32)> {
+    let crossings = wire_crossings(wire1, wire2);
+    crossings
+        .iter()
+        .min_by_key(|(x, y)| central_distance(*x, *y))
+        .cloned()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn parse_turn_should_parse_valid_turns() {
+        assert_eq!(parse_turn("U35"), Ok(Turn::Ver(35)));
+        assert_eq!(parse_turn("R2"), Ok(Turn::Hor(2)));
+        assert_eq!(parse_turn("D2"), Ok(Turn::Ver(-2)));
+        assert_eq!(parse_turn("L123"), Ok(Turn::Hor(-123)));
+    }
+
+    #[test]
+    fn parse_turn_should_fail_on_invalid_turns() {
+        assert!(parse_turn("U").is_err());
+        assert!(parse_turn("Ü35").is_err());
+        assert!(parse_turn("Ublubb").is_err());
+        assert!(parse_turn("W20").is_err());
+    }
+
+    #[test]
+    fn wire_crossings_should_work_for_example() {
+        // given
+        let (wire1, wire2) =
+            parse_wires(&["R8,U5,L5,D3", "U7,R6,D4,L4"]).expect("Expected valid wires");
+
+        // when
+        let crossings = wire_crossings(&wire1, &wire2);
+
+        // then
+        assert_eq!(crossings.len(), 2);
+        assert!(crossings.contains(&(3, 3)));
+    }
+
+    #[test]
+    fn closest_crossing_should_work_for_first_example() {
+        // given
+        let (wire1, wire2) =
+            parse_wires(&["R8,U5,L5,D3", "U7,R6,D4,L4"]).expect("Expected valid wires");
+
+        // when
+        let crossing = closest_crossing(&wire1, &wire2);
+
+        // then
+        assert_eq!(crossing, Some((3, 3)));
+    }
+
+    #[test]
+    fn closest_crossing_should_work_for_second_example() {
+        // given
+        let (wire1, wire2) = parse_wires(&[
+            "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+            "U62,R66,U55,R34,D71,R55,D58,R83",
+        ])
+        .expect("Expected valid wires");
+
+        // when
+        let crossing = closest_crossing(&wire1, &wire2).map(|(x, y)| central_distance(x, y));
+
+        // then
+        assert_eq!(crossing, Some(159));
+    }
+
+    #[test]
+    fn closest_crossing_should_work_for_third_example() {
+        // given
+        let (wire1, wire2) = parse_wires(&[
+            "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+            "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+        ])
+        .expect("Expected valid wires");
+
+        // when
+        let crossing = closest_crossing(&wire1, &wire2).map(|(x, y)| central_distance(x, y));
+
+        // then
+        assert_eq!(crossing, Some(135));
+    }
+}

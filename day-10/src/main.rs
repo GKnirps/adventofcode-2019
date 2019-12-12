0c15ef503f63cd0949dd6e6c8baedf2e82933c95
Guido Knips <guido.knips@posteo.de>
2019-12-12 21:29:10 +0100

    Add solution for day 10 puzzle 2
    While I was satisfied with the elegance of my solution
    for puzzle 1, I am rather dissatisfied with my solution
    for puzzle 2. It works, though. Give me some break, I was
    tired.

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -1,5 +1,5 @@
-use std::cmp;
-use std::collections::HashSet;
+use std::cmp::{self, Ordering};
+use std::collections::{HashMap, HashSet};
 use std::env;
 use std::fs::File;
 use std::io::{BufReader, Read};
@@ -15,10 +15,20 @@ fn main() -> Result<(), String> {
     let asteroids = parse_map(&lines);
 
     match max_visible_asteroids(&asteroids) {
-        Some(n) => println!(
-            "The best spot for a monitoring station can detect {} asteroids",
-            n
-        ),
+        Some((n, x, y)) => {
+            println!(
+                "The best spot for a monitoring station is ({}, {}) can detect {} asteroids",
+                x, y, n
+            );
+            if let Some((x200, y200)) = find_200th_asteroid(x, y, &asteroids) {
+                println!(
+                    "The 200th asteroid vaporized by the laser is at ({}, {}), result is {}",
+                    x200,
+                    y200,
+                    x200 * 100 + y200
+                );
+            }
+        }
         None => println!("There are no asteroids at all!"),
     }
 
@@ -79,11 +89,96 @@ fn count_visible_asteroids(x_base: i32, y_base: i32, asteroids: &[(i32, i32)]) -
         .len()
 }
 
-fn max_visible_asteroids(asteroids: &[(i32, i32)]) -> Option<usize> {
+fn max_visible_asteroids(asteroids: &[(i32, i32)]) -> Option<(usize, i32, i32)> {
     asteroids
         .iter()
-        .map(|(x, y)| count_visible_asteroids(*x, *y, asteroids))
-        .max()
+        .map(|(x, y)| (count_visible_asteroids(*x, *y, asteroids), *x, *y))
+        .max_by_key(|(count, _, _)| *count)
+}
+
+fn asteroids_by_direction(
+    x_base: i32,
+    y_base: i32,
+    asteroids: &[(i32, i32)],
+) -> HashMap<(i32, i32), Vec<(i32, i32)>> {
+    let mut ast_by_dir = asteroids.iter().filter(|a| **a != (x_base, y_base)).fold(
+        HashMap::with_capacity(asteroids.len()),
+        |mut map, (x, y)| {
+            let dir = normalize(x - x_base, y - y_base);
+            map.entry(dir)
+                .or_insert_with(|| Vec::with_capacity(asteroids.len()))
+                .push((*x, *y));
+            map
+        },
+    );
+
+    for list in ast_by_dir.values_mut() {
+        list.sort_by_key(|(x, y)| (x - x_base).abs() + (y - y_base).abs());
+    }
+    ast_by_dir
+}
+
+fn find_200th_asteroid(x_base: i32, y_base: i32, asteroids: &[(i32, i32)]) -> Option<(i32, i32)> {
+    let by_dir = asteroids_by_direction(x_base, y_base, asteroids);
+    let mut entries: Vec<((i32, i32), Vec<(i32, i32)>)> = by_dir.into_iter().collect();
+    entries.sort_by(|(dir1, _), (dir2, _)| compare_vec_angle(*dir1, *dir2));
+    let targets: Vec<Vec<(i32, i32)>> = entries.into_iter().map(|(_, a)| a).collect();
+
+    let max_length = targets.iter().map(|v| v.len()).max()?;
+    let mut count: usize = 0;
+
+    for index in 0..max_length {
+        for target in &targets {
+            if let Some(asteroid) = target.get(index) {
+                if count < 199 {
+                    count += 1;
+                } else {
+                    return Some(*asteroid);
+                }
+            }
+        }
+    }
+
+    None
+}
+
+fn scal_prod((x1, y1): (i32, i32), (x2, y2): (i32, i32)) -> f64 {
+    (x1 * x2 + y1 * y2) as f64
+}
+
+fn compare_vec_angle(v1: (i32, i32), v2: (i32, i32)) -> Ordering {
+    let (x1, _) = v1;
+    let (x2, _) = v2;
+    let len1 = scal_prod(v1, v1).sqrt();
+    let len2 = scal_prod(v2, v2).sqrt();
+    if len1 == 0.0 || len2 == 0.0 {
+        // not really an angle to calculate, make zero length vectors less than everything else as fallback
+        return len1
+            .partial_cmp(&len2)
+            .expect("We have a weird floating point value here");
+    }
+
+    let base: (i32, i32) = (0, -1);
+
+    let a1 = (scal_prod(base, v1) / len1).acos();
+    let a2 = (scal_prod(base, v2) / len2).acos();
+
+    let full_angle1 = if x1 < 0 {
+        std::f64::consts::PI * 2.0 - a1
+    } else {
+        a1
+    };
+    let full_angle2 = if x2 < 0 {
+        std::f64::consts::PI * 2.0 - a2
+    } else {
+        a2
+    };
+
+    // this is the reason I wanted to avoid floating point numbers at first.
+    // I could handle that without a panic in theory, but I have no nerve for that now.
+    full_angle1
+        .partial_cmp(&full_angle2)
+        .expect("We have a weird floating point value here")
 }
 
 #[cfg(test)]
@@ -107,7 +202,6 @@ mod test {
 
     #[test]
     fn test_normalize() {
-        assert_eq!(normalize(0, 0), (0, 0));
         assert_eq!(normalize(42, 0), (1, 0));
         assert_eq!(normalize(-42, 0), (-1, 0));
         assert_eq!(normalize(0, 42), (0, 1));
@@ -129,7 +223,7 @@ mod test {
         let result = max_visible_asteroids(&asteroids).expect("Expected at least one asteroid");
 
         // then
-        assert_eq!(result, 8);
+        assert_eq!(result, (8, 3, 4));
     }
 
     #[test]
@@ -153,6 +247,71 @@ mod test {
         let result = max_visible_asteroids(&asteroids).expect("Expected at least one asteroid");
 
         // then
-        assert_eq!(result, 33);
+        assert_eq!(result, (33, 5, 8));
+    }
+
+    #[test]
+    fn test_compare_vec_angle() {
+        // given
+        let v_pairs = &[
+            ((0, 0), (0, 0), Ordering::Equal),
+            ((0, -1), (-1, -1), Ordering::Less),
+            ((0, -1), (-1, 0), Ordering::Less),
+            ((0, -1), (-5, 0), Ordering::Less),
+            ((0, -1), (-1, 1), Ordering::Less),
+            ((0, -1), (0, -1), Ordering::Equal),
+            ((0, -1), (0, 0), Ordering::Greater), // by my definition, actually, this is not defined
+            ((0, -1), (0, 1), Ordering::Less),
+            ((0, -1), (1, -1), Ordering::Less),
+            ((0, -1), (1, 0), Ordering::Less),
+            ((0, -1), (1, 1), Ordering::Less),
+            ((1, 1), (2000, 2000), Ordering::Equal),
+            ((1, 1), (1, -2), Ordering::Greater),
+            ((1, 1), (1, 2), Ordering::Less),
+            ((1, 1), (2, 1), Ordering::Greater),
+            ((1, 1), (-1, 1), Ordering::Less),
+            ((-1, -12), (-1, -11), Ordering::Greater),
+            ((-10, -1), (-8, 1), Ordering::Greater),
+        ];
+
+        for (v1, v2, ord) in v_pairs {
+            println!("Comparing {:?} and {:?}", v1, v2);
+            assert_eq!(compare_vec_angle(*v1, *v2), *ord);
+            assert_eq!(compare_vec_angle(*v2, *v1), ord.reverse());
+        }
+    }
+
+    #[test]
+    fn test_example_laser() {
+        // given
+        let raw_map = &[
+            ".#..##.###...#######",
+            "##.############..##.",
+            ".#.######.########.#",
+            ".###.#######.####.#.",
+            "#####.##.#.##.###.##",
+            "..#####..#.#########",
+            "####################",
+            "#.####....###.#.#.##",
+            "##.#################",
+            "#####.##.###..####..",
+            "..######..##.#######",
+            "####.##.####...##..#",
+            ".#####..#.######.###",
+            "##...#.##########...",
+            "#.##########.#######",
+            ".####.#.###.###.#.##",
+            "....##.##.###..#####",
+            ".#.#.###########.###",
+            "#.#.#.#####.####.###",
+            "###.##.####.##.#..##",
+        ];
+        let asteroids = parse_map(raw_map);
+
+        // when
+        let result = find_200th_asteroid(11, 13, &asteroids);
+
+        // then
+        assert_eq!(result, Some((8, 2)));
     }
 }

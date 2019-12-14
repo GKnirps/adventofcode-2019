a469035589c635089953cd62c049d8d3d3c737d1
Guido Knips <guido.knips@posteo.de>
2019-12-14 09:46:39 +0100

    Add solution for day 13 puzzle 2

--- day-13/src/main.rs
+++ day-13/src/main.rs
@@ -1,4 +1,4 @@
-use intcode::{parse, run_program, State};
+use intcode::{parse, run_program, ReturnStatus, State};
 use std::collections::HashMap;
 use std::env;
 use std::fs::File;
@@ -10,14 +10,18 @@ fn main() -> Result<(), String> {
         .nth(1)
         .ok_or_else(|| "No file name given.".to_owned())?;
     let content = read_file(&Path::new(&filename)).map_err(|e| e.to_string())?;
-    let program = parse(&content)?;
+    let mut program = parse(&content)?;
 
-    let (_, _, initial_output) = run_program(State::new(program), &[])?;
+    let (_, _, initial_output) = run_program(State::new(program.clone()), &[])?;
     let initial_paint = translate_paint_output(&initial_output);
     println!(
         "Initially, there are {} block tiles.",
         initial_paint.values().filter(|t| **t == 2).count()
     );
+
+    program[0] = 2;
+    let end_state = run_game(program)?;
+    evaluate_result(&end_state);
 
     Ok(())
 }
@@ -36,4 +40,61 @@ fn translate_paint_output(output: &[isize]) -> HashMap<(isize, isize), isize> {
         .chunks_exact(3)
         .map(|chunk| ((chunk[0], chunk[1]), chunk[2]))
         .collect()
+}
+
+fn run_game(program: Vec<isize>) -> Result<HashMap<(isize, isize), isize>, String> {
+    let mut display: HashMap<(isize, isize), isize> = HashMap::with_capacity(1024);
+    let mut process = State::new(program);
+    let mut input: isize = 0;
+
+    loop {
+        let (state, status, output) = run_program(process, &[input])?;
+        for (x, y, value) in output
+            .chunks_exact(3)
+            .map(|chunk| (chunk[0], chunk[1], chunk[2]))
+        {
+            display.insert((x, y), value);
+        }
+        if status == ReturnStatus::Halt {
+            return Ok(display);
+        }
+        process = state;
+
+        let ((paddle_x, _), (ball_x, _)) = find_paddle_and_ball(&display)
+            .ok_or_else(|| "Unable to find paddle and ball on display!".to_owned())?;
+        input = (ball_x - paddle_x).signum();
+    }
+}
+
+fn find_paddle_and_ball(
+    display: &HashMap<(isize, isize), isize>,
+) -> Option<((isize, isize), (isize, isize))> {
+    let paddle = display
+        .iter()
+        .filter(|((x, y), value)| !(*x == -1 && *y == 0) && **value == 3)
+        .map(|(pos, _)| *pos)
+        .next()?;
+    let ball = display
+        .iter()
+        .filter(|((x, y), value)| !(*x == -1 && *y == 0) && **value == 4)
+        .map(|(pos, _)| *pos)
+        .next()?;
+
+    Some((paddle, ball))
+}
+
+fn evaluate_result(display: &HashMap<(isize, isize), isize>) {
+    let leftover_blocks = display
+        .iter()
+        .filter(|((x, y), value)| !(*x == -1 && *y == 0) && **value == 2)
+        .count();
+    if leftover_blocks > 0 {
+        println!(
+            "You lost the game! There are still {} blocks left!",
+            leftover_blocks
+        );
+    }
+    if let Some(points) = display.get(&(-1, 0)) {
+        println!("Result: {} points", points);
+    }
 }

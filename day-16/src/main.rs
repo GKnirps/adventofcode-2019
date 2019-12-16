d2ec01f55872a027933b315050c77a23eaa3f4d8
Guido Knips <guido.knips@posteo.de>
2019-12-16 20:49:11 +0100

    Add solution for day 16 puzzle 2
    - tricky
    - not the most beautiful rust code I've written
    - but hey, it is 23:55 and I have to work tomorrow

--- day-16/src/main.rs
+++ day-16/src/main.rs
@@ -10,9 +10,16 @@ fn main() -> Result<(), String> {
     let content = read_file(&Path::new(&filename)).map_err(|e| e.to_string())?;
     let input = parse_input(&content);
 
-    let after_100_phases = fft(input, 100);
+    let after_100_phases = fft(input.clone(), 100);
     print!("First eight digits after 100 phases: ");
     for d in after_100_phases.iter().take(8) {
+        print!("{}", d);
+    }
+    println!();
+
+    let large_result = fft_large_input(&input, 10000)
+        .ok_or_else(|| "Sorry, but this would take years".to_owned())?;
+    for d in large_result {
         print!("{}", d);
     }
     println!();
@@ -53,6 +60,7 @@ fn fft_phase(input: &[i32]) -> Vec<i32> {
             input
                 .iter()
                 .enumerate()
+                .skip(output_i)
                 .map(|(input_i, input_v)| input_v * pattern_for(output_i, input_i))
                 .sum::<i32>()
                 .abs()
@@ -65,6 +73,57 @@ fn fft_phase(input: &[i32]) -> Vec<i32> {
 static BASE_PATTERN: [i32; 4] = [0, 1, 0, -1];
 fn pattern_for(output_i: usize, input_i: usize) -> i32 {
     BASE_PATTERN[((input_i + 1) / (output_i + 1)) % BASE_PATTERN.len()]
+}
+
+fn fft_phase_upper_half(upper: &[i32]) -> Vec<i32> {
+    // idea: the result of the upper half only depends on the upper half.
+    // the first output of the upper half is a sum of the input at that point plus all inputs
+    // after that
+    // the second output of the upper half is the sum of the input at that point plus all
+    // inputs after that and so on => we can just use a cumulative sum.
+    let mut result: Vec<i32> = Vec::with_capacity(upper.len());
+    let mut prev = 0;
+    for v in upper.iter().rev() {
+        prev = (prev + v) % 10;
+        result.push(prev);
+    }
+    result.reverse();
+    result
+}
+
+fn get_output_offset(input: &[i32]) -> usize {
+    input.iter().take(7).fold(0, |n, d| n * 10 + *d as usize)
+}
+
+// im not calling this ffft
+fn fft_large_input(input: &[i32], n_repeat: usize) -> Option<Vec<i32>> {
+    // It is not feasible to calculate the fft for large inputs.
+    // However, we may have some advantages:
+    // - the 2nd half is cheap to calculate
+    // - the 2nd half is independent of the first half
+    // => idea: if the result we are looking for lies in the second half, we win!
+    let offset = (input.len() * n_repeat) / 2;
+    let output_offset = get_output_offset(input);
+
+    if output_offset < offset {
+        // I know of no efficient way to do this.
+        return None;
+    }
+
+    let mut current_output: Vec<i32> = input
+        .iter()
+        .cycle()
+        .skip(offset)
+        .take(input.len() * n_repeat - offset)
+        .cloned()
+        .collect();
+
+    for _ in 0..100 {
+        current_output = fft_phase_upper_half(&current_output);
+    }
+    current_output
+        .get((output_offset - offset)..(output_offset - offset + 8))
+        .map(|o| o.to_vec())
 }
 
 #[cfg(test)]
@@ -116,6 +175,14 @@ mod test {
         assert_eq!(
             fft(parse_input("69317163492948606335995924319873"), 100)[0..8],
             [5, 2, 4, 3, 2, 1, 3, 3]
+        );
+    }
+
+    #[test]
+    fn test_puzzle2_example() {
+        assert_eq!(
+            fft_large_input(&parse_input("03036732577212944063491565474664"), 10000),
+            Some(vec![8, 4, 4, 6, 2, 0, 2, 6])
         );
     }
 }

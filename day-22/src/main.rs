8c06bff13c2d908834e9b67e0a428b7ed26615a9
Guido Knips <guido.knips@posteo.de>
2022-12-20 17:38:07 +0100

    Add solution for day 22, part 2

--- day-22/src/main.rs
+++ day-22/src/main.rs
@@ -11,8 +11,23 @@ fn main() -> Result<(), String> {
     let lines: Vec<&str> = content.split('\n').collect();
     let techniques: Vec<Technique> = parse_techniques(&lines)?;
 
-    let index = apply_techniques_to_index(&techniques, 2019, 10007);
-    println!("After shuffling one time, card 2019 is at index: {}", index);
+    let joined_technique_10007 = join_techniques(&techniques, 10007);
+    let index = apply_joined_technique_to_index(joined_technique_10007, 2019, 10007);
+    println!("After shuffling one time, card 2019 is at index: {index}");
+
+    let large_stack_size: i64 = 119_315_717_514_047;
+    let shuffle_rounds: i64 = 101_741_582_076_661;
+
+    let end_index: i64 = 2020;
+
+    let joined_technique_large = join_techniques(&techniques, large_stack_size);
+    let reverted = reverse_index_a_ridiculous_number_of_times(
+        joined_technique_large,
+        end_index,
+        large_stack_size,
+        shuffle_rounds,
+    );
+    println!("The start index was {reverted}");
 
     Ok(())
 }
@@ -29,8 +44,8 @@ fn read_file(path: &Path) -> std::io::Result<String> {
 #[derive(Clone, Copy, PartialEq, PartialOrd, Eq, Ord, Debug, Hash)]
 enum Technique {
     DealIntoNew,
-    Cut(isize),
-    DealWithInc(usize),
+    Cut(i64),
+    DealWithInc(i64),
 }
 
 fn parse_techniques(lines: &[&str]) -> Result<Vec<Technique>, String> {
@@ -49,41 +64,97 @@ fn parse_technique(line: &str) -> Result<Technique, String> {
         Ok(Technique::DealIntoNew)
     } else if line.starts_with(CUT_NAME) && line.len() > CUT_NAME.len() {
         let (_, cut_substr) = line.split_at(CUT_NAME.len());
-        let cut: isize = cut_substr.parse::<isize>().map_err(|e| e.to_string())?;
+        let cut: i64 = cut_substr.parse::<i64>().map_err(|e| e.to_string())?;
         Ok(Technique::Cut(cut))
     } else if line.starts_with(DEAL_WITH_INCREMENT_NAME)
         && line.len() > DEAL_WITH_INCREMENT_NAME.len()
     {
         let (_, increment_substr) = line.split_at(DEAL_WITH_INCREMENT_NAME.len());
-        let increment: usize = increment_substr
-            .parse::<usize>()
-            .map_err(|e| e.to_string())?;
+        let increment: i64 = increment_substr.parse::<i64>().map_err(|e| e.to_string())?;
         Ok(Technique::DealWithInc(increment))
     } else {
         Err("Unknown technique".to_owned())
     }
 }
 
-fn apply_techniques_to_index(techniques: &[Technique], index: usize, stack_size: usize) -> usize {
-    techniques.iter().fold(index, |perm, tech| {
-        apply_technique_to_index(*tech, perm, stack_size)
-    })
+fn apply_joined_technique_to_index(joined: JoinedTechnique, index: i64, stack_size: i64) -> i64 {
+    (index * joined.a + joined.b).rem_euclid(stack_size)
 }
 
-fn apply_technique_to_index(technique: Technique, index: usize, stack_size: usize) -> usize {
-    match technique {
-        Technique::DealIntoNew => stack_size - 1 - index,
-        Technique::Cut(i) => cut_index(index, stack_size, i),
-        Technique::DealWithInc(n) => (index * n) % stack_size,
+// represents index shuffle of the form (i*a + b) % stack_size
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+struct JoinedTechnique {
+    a: i64,
+    b: i64,
+}
+
+// this works fine if the stack size is a prime number (which they are for both part 1 and 2)
+fn join_techniques(techniques: &[Technique], stack_size: i64) -> JoinedTechnique {
+    techniques
+        .iter()
+        .map(|technique| match technique {
+            Technique::DealIntoNew => JoinedTechnique {
+                a: -1,
+                b: stack_size - 1,
+            },
+            Technique::Cut(i) => JoinedTechnique { a: 1, b: -i },
+            Technique::DealWithInc(n) => JoinedTechnique { a: *n, b: 0 },
+        })
+        .fold(
+            JoinedTechnique { a: 1, b: 0 },
+            |JoinedTechnique { a: a1, b: b1 }, JoinedTechnique { a: a2, b: b2 }| JoinedTechnique {
+                a: (a1 * a2) % stack_size,
+                b: (a2 * b1 + b2) % stack_size,
+            },
+        )
+}
+
+fn reverse_index_a_ridiculous_number_of_times(
+    technique: JoinedTechnique,
+    end_index: i64,
+    stack_size: i64,
+    shuffle_rounds: i64,
+) -> i64 {
+    // I had to look up a lot of modular arithmetic on wikipedia. I learned all of it at some
+    // point, but I forgot most of it.
+    // Of course, this only works if stack_size is a prime number, but fortunately, this is the
+    // case.
+    let am = pow_mod(
+        technique.a as i128,
+        shuffle_rounds as i128,
+        stack_size as i128,
+    ) as i64;
+    let (_, _, am_inv) = extended_euclid(stack_size, am);
+    let (_, _, d) = extended_euclid(stack_size, (1 - technique.a).rem_euclid(stack_size));
+    let b = (technique.b as i128
+        * ((1 - am).rem_euclid(stack_size) as i128 * d as i128).rem_euclid(stack_size as i128))
+    .rem_euclid(stack_size as i128) as i64;
+    ((end_index as i128 - b as i128).rem_euclid(stack_size as i128) * am_inv as i128)
+        .rem_euclid(stack_size as i128) as i64
+}
+
+fn pow_mod(mut base: i128, mut exp: i128, m: i128) -> i128 {
+    if m == 1 {
+        return 0;
     }
+    let mut result = 1;
+    base %= m;
+    while exp > 0 {
+        if exp % 2 == 1 {
+            result = result * base % m;
+        }
+        exp >>= 1;
+        base = base * base % m
+    }
+    result
 }
 
-fn cut_index(index: usize, stack_size: usize, icutoff: isize) -> usize {
-    let cutoff: usize = (icutoff % stack_size as isize + stack_size as isize) as usize % stack_size;
-    if index < cutoff {
-        index + (stack_size - cutoff)
+fn extended_euclid(a: i64, b: i64) -> (i64, i64, i64) {
+    if b == 0 {
+        (a, 1, 0)
     } else {
-        index - cutoff
+        let (d, s, t) = extended_euclid(b, a % b);
+        (d, t, s - (a / b) * t)
     }
 }
 
@@ -110,48 +181,7 @@ mod test {
     }
 
     #[test]
-    fn test_cut() {
-        assert_eq!(
-            cut(vec!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9), 3),
-            vec!(3, 4, 5, 6, 7, 8, 9, 0, 1, 2)
-        );
-        assert_eq!(
-            cut(vec!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9), -4),
-            vec!(6, 7, 8, 9, 0, 1, 2, 3, 4, 5)
-        );
-    }
-
-    #[test]
-    fn test_deal_with_increment() {
-        assert_eq!(
-            deal_with_increment(vec!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9), 3),
-            vec!(0, 7, 4, 1, 8, 5, 2, 9, 6, 3)
-        );
-    }
-
-    #[test]
-    fn test_example_4() {
-        // given
-        let lines = &[
-            "deal into new stack",
-            "cut -2",
-            "deal with increment 7",
-            "cut 8",
-            "cut -4",
-            "deal with increment 7",
-            "cut 3",
-            "deal with increment 9",
-            "deal with increment 3",
-            "cut -1",
-        ];
-        let techniques = parse_techniques(lines).expect("Expected valid techniques");
-
-        let input: Vec<usize> = (0..10).collect();
-
-        // when
-        let result = apply_techniques(&techniques, input);
-
-        // then
-        assert_eq!(&result, &[9, 2, 5, 8, 1, 4, 7, 0, 3, 6]);
+    fn pow_mod_works_correctly() {
+        assert_eq!(pow_mod(3, 4, 11), 4);
     }
 }
